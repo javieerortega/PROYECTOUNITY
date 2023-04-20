6e41ec9ec7b28c2dc0daae9a7ba9f58e2e01b89d
javieerortega <Javierortegacobos@gmail.com>
2023-04-20 17:51:42 +0200

    valeee

--- Scenes/scripts/Codigocartas.cs
+++ Scenes/scripts/Codigocartas.cs
@@ -5,44 +5,56 @@ using UnityEngine.UI;
 
 public class Codigocartas : MonoBehaviour
 {
-    //private List<Image> selectedImages = new List<Image>();
-    //private int maxSelectedImages = 2;
+    public List<GameObject> selectedImages = new List<GameObject>();
+    private int maxSelectedImages = 2;
+
     void Start()
     {
 
+        {
+
+        }
     }
+
     void Update()
     {
 
     }
+
     public void OnImageClick(GameObject image)
     {
+        // Si ya se han seleccionado el número máximo de imágenes, desactivamos todas las imágenes y limpiamos la lista de selección
+        if (selectedImages.Count == maxSelectedImages)
+        {
+            bool match = selectedImages[0].GetComponentInChildren<Image>().sprite.name == selectedImages[1].GetComponentInChildren<Image>().sprite.name;
+            foreach (GameObject selectedImage in selectedImages)
+            {
+                if (match)
+                {
+                    Destroy(selectedImage);
+                }
+                else
+                {
+                    selectedImage.SetActive(false);
+                }
+            }
+            selectedImages.Clear();
+        }
 
-        image.SetActive(true);
-            
+        // Añadimos o eliminamos la imagen seleccionada de la lista de imágenes seleccionadas
+        if (selectedImages.Contains(image))
+        {
+            selectedImages.Remove(image);
+        }
+        else
+        {
+            selectedImages.Add(image);
+        }
 
-        //if (selectedImages.Contains(image))
-        //{
-        //    selectedImages.Remove(image);
-        //    image.gameObject.SetActive(false); 
-        //}
-      
-        //else if (selectedImages.Count < maxSelectedImages)
-        //{
-        //    selectedImages.Add(image);
-        //    image.gameObject.SetActive(true);
-        //}
-
-        //if (selectedImages.Count == maxSelectedImages)
-        //{
-           
-        //    foreach (Image selectedImage in selectedImages)
-        //    {
-        //        selectedImage.gameObject.SetActive(false);
-        //    }
-           
-        //    selectedImages.Clear();
-        //}
+        // Activamos o desactivamos las imágenes seleccionadas
+        foreach (GameObject selectedImage in selectedImages)
+        {
+            selectedImage.SetActive(true);
+        }
     }
-
 }
