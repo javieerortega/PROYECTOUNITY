6e381aba6bb591cf8db9c7d23f1b3c9e3f19c485
javieerortega <Javierortegacobos@gmail.com>
2023-04-20 13:36:49 +0200

    bebesitadimeagu

--- Scenes/scripts/Codigocartas.cs
+++ Scenes/scripts/Codigocartas.cs
@@ -5,42 +5,38 @@ using UnityEngine.UI;
 
 public class Codigocartas : MonoBehaviour
 {
-    private List<Image> selectedImages = new List<Image>();
+    public List<Image> selectedImages = new List<Image>();
     private int maxSelectedImages = 2;
     void Start()
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
 
     }
     public void OnImageClick(Image image)
     {
-        // Si la imagen ya está seleccionada, deseleccionarla y volverla a la posición inicial
         if (selectedImages.Contains(image))
         {
             selectedImages.Remove(image);
-            image.gameObject.SetActive(false); // Opcional: volver a la posición inicial
+            image.gameObject.SetActive(false); 
         }
-        // Si la imagen no está seleccionada y no se han seleccionado el máximo de imágenes, seleccionarla
+      
         else if (selectedImages.Count < maxSelectedImages)
         {
             selectedImages.Add(image);
             image.gameObject.SetActive(true);
         }
 
-        // Si se han seleccionado el máximo de imágenes permitidas, hacer algo con ellas
         if (selectedImages.Count == maxSelectedImages)
         {
-            // Hacer algo con las imágenes seleccionadas, por ejemplo, voltearlas
+           
             foreach (Image selectedImage in selectedImages)
             {
                 selectedImage.gameObject.SetActive(false);
             }
-            // Limpiar la lista de imágenes seleccionadas
+           
             selectedImages.Clear();
         }
     }
